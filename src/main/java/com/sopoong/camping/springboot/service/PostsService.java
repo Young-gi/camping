9e2f4056d1522c020b7d74eb987d85989e12c9ca
YounggikimA <rladudrl286@naver.com>
2020-04-29 16:03:40 +0900

    Revert "API CRUD / oauth"
    This reverts commit 68c20695

--- src/main/java/com/sopoong/camping/springboot/service/PostsService.java
+++ src/main/java/com/sopoong/camping/springboot/service/PostsService.java
@@ -2,16 +2,12 @@ package com.sopoong.camping.springboot.service;
 
 import com.sopoong.camping.springboot.domain.posts.Posts;
 import com.sopoong.camping.springboot.domain.posts.PostsRepository;
-import com.sopoong.camping.springboot.web.dto.PostsListResponseDto;
 import com.sopoong.camping.springboot.web.dto.PostsResponseDto;
 import com.sopoong.camping.springboot.web.dto.PostsSaveRequestDto;
 import com.sopoong.camping.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,23 +32,6 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
-    }
-
-    @Transactional(readOnly = true)
-    public List<PostsListResponseDto> findAllDesc() {
-        return postsRepository.findAllDesc().stream()
-                .map(PostsListResponseDto::new)
-                .collect(Collectors.toList());
-//              .map(posts -> new PostsListResponseDto(posts))
-
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id="+id));
-        postsRepository.delete(posts);
-//        return id;
     }
 
 
